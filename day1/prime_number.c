cb053dd449139ef0621f55416bbea359b3f3696c
Antima2004 <antimamishra277@gmail.com>
2026-02-01 22:30:27 +0530

    prime_number practice

---
+++ day1/prime_number.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int is_prime(int num) {
+    if (num <= 1) return 0; // 0 and 1 are not prime numbers
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return 0; // Found a divisor, not prime
+    }
+    return 1; // No divisors found, it's prime
+}
+int main() {
+    int number;
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    if (is_prime(number)) {
+        printf("%d is a prime number.\n", number);
+    } else {
+        printf("%d is not a prime number.\n", number);
+    }
+
+    return 0;
+}
